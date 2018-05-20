a10393d87d429c8a8207beccf733cca963c4805d
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-05-20 11:09:45 +0200

    02: use function

---
+++ quiz_2.py
@@ -0,0 +1,7 @@
+def show_question(question, correct_answer, options):
+    print(question)
+    print(options)
+    print(correct_answer)
+
+show_question("Quelle est la vitesse de pointe d'un thon?", "80 km/h", ["20 km/h", "40 km/h", "80 km/h"])
+show_question("Les poissons ont-ils tous des écailles?", "Non", ["Oui", "Non"])
