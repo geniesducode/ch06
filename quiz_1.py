8e6252dba74bd2b9f3b738102006b8298e08ee78
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-05-20 11:02:53 +0200

    00: initial version

---
+++ quiz_1.py
@@ -0,0 +1,7 @@
+print("Quelle est la vitesse de pointe d'un thon?")
+print("- 20 km/h")
+print("- 40 km/h")
+print("- 80 km/h")
+print("Entrez une réponse:")
+answer = input()
+print("Vous avez répondu " + answer)
