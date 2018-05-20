4b2565892a89dec7b93137ebd5a33f7f005706ad
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-05-20 11:05:51 +0200

    01: add answer check

--- quiz_1.py
+++ quiz_1.py
@@ -4,4 +4,12 @@ print("- 40 km/h")
 print("- 80 km/h")
 print("Entrez une réponse:")
 answer = input()
-print("Vous avez répondu " + answer)
+
+if answer == "80 km/h":
+    score = 1
+    print("Bravo!")
+else:
+    score = 0
+    print("La réponse n'est pas correcte")
+
+print("Vous avez répondu correctement à " + str(score) + " question sur 1")
