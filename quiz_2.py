70ca991c38cf97d8cb8dd6586b1b5791460052d8
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-05-20 11:11:16 +0200

    03: add interactivity

--- quiz_2.py
+++ quiz_2.py
@@ -1,7 +1,14 @@
 def show_question(question, correct_answer, options):
     print(question)
     print(options)
-    print(correct_answer)
+
+    print("Entrez une réponse:")
+    answer = input()
+
+    if answer == correct_answer:
+        print("Bravo!")
+    else:
+        print("La réponse n'est pas correcte")
 
 show_question("Quelle est la vitesse de pointe d'un thon?", "80 km/h", ["20 km/h", "40 km/h", "80 km/h"])
 show_question("Les poissons ont-ils tous des écailles?", "Non", ["Oui", "Non"])
